cc27b0a68e81d49288d6d982d819dd1a72fe2b7d
maharjananel <maharjananel@gmail.com>
2014-03-28 18:47:40 -0400

    php html bootstrap
    wow

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+define('MAINDIR',dirname(__FILE__) . '/');
+?>
+
+<?php
+include(MAINDIR . 'header.php');
+?>
+
+<?php
+include(MAINDIR . 'layouthome.php');
+?>
+
+<?php
+include(MAINDIR . 'footer.php');
+?>
---
+++ nav.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<!-- Navigation-->
+
+  <a class="navbar-brand" href="#">Brand</a>
+  <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+							  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							  </button>
+			</div>
+   	  <!-- for links-->
+   	<div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1" >
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#">portfolio</a></li>
+	   
+	   
+							<li class="dropdown">
+									  <a href="#" class="dropdown-toggle" data-toggle="dropdown">More<b class="caret"></b></a>
+											  <ul class="dropdown-menu">
+												<li><a href="#">Contact us</a></li>
+												<li class="divider"></li>
+												<li><a href="#">Location</a></li>
+												
+											  </ul>
+							</li>
+		
+		  </ul>
+   	</div><!-- /.navbar-collapse -->
+   		
+   </div> <!--  end of container fluid -->
+   </nav>
